69176906d336fe09664d372c31b8430a5c6f8c67
Dave Raring <d@7pi.co>
2023-04-28 16:50:05 -0500

    updating context

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 locals {
-  enabled = module.this.enabled
+  enabled = module.context.enabled
   public_key_filename = format(
     "%s/%s",
     var.ssh_public_key_path,
-    coalesce(var.ssh_public_key_file, join("", [module.this.id, var.public_key_extension]))
+    coalesce(var.ssh_public_key_file, join("", [module.context.id, var.public_key_extension]))
   )
 
   private_key_filename = format(
     "%s/%s%s",
     var.ssh_public_key_path,
-    module.this.id,
+    module.context.id,
     var.private_key_extension
   )
 }
 
 resource "aws_key_pair" "imported" {
   count      = local.enabled && var.generate_ssh_key == false ? 1 : 0
-  key_name   = module.this.id
+  key_name   = module.context.id
   public_key = file(local.public_key_filename)
-  tags       = module.this.tags
+  tags       = module.context.tags
 }
 
 resource "tls_private_key" "default" {
@@ -29,9 +29,9 @@ resource "tls_private_key" "default" {
 resource "aws_key_pair" "generated" {
   count      = local.enabled && var.generate_ssh_key == true ? 1 : 0
   depends_on = [tls_private_key.default]
-  key_name   = module.this.id
+  key_name   = module.context.id
   public_key = tls_private_key.default[0].public_key_openssh
-  tags       = module.this.tags
+  tags       = module.context.tags
 }
 
 resource "local_file" "public_key_openssh" {
